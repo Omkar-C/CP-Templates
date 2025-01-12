48655c654278fb0446ddeea8bb5d4c4d8327b682
Omkar Chorghe <omkarchorghe22@gmail.com>
2025-01-12 19:57:33 +0530

    Update dijkstra

--- Graph/Single Source Shortest Path/dijkstra.cpp
+++ Graph/Single Source Shortest Path/dijkstra.cpp
@@ -14,37 +14,47 @@ using pi = pair<int,int>;
 #define s second
 #define mp make_pair
 
-const int INF = 1e16;
-
-class dijkstra{
+class Dijkstra{
     private:
-        vector<long long> distance;
-        vector<vector<pair<int,int>>> graph;
+        vector<ll> distance;
+        vector<vector<pair<ll,ll>>> graph;
         vector<long long> parent;
         int n;
-    public:
-        dijkstra(vector<vector<pair<int,int>>> &adjList) {
+        int sourceVertex;
+        ll INF = 1e16;
+
+        void init(vector<vector<pair<ll,ll>>> &adjList) {
             graph = adjList;
             n = graph.size();
             distance.assign(n, INF);
             parent.assign(n, -1);
         }
 
+    public:
+        Dijkstra(vector<vector<pair<ll,ll>>> &adjList) {
+            init(adjList);
+        }
+
+        Dijkstra(vector<vector<pair<ll,ll>>> &adjList, ll inf) {
+            INF = inf;
+            init(adjList);
+        }
+
         void dijkstraAlgo(int source) {
-            distance[source] = 0;
-            priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
-            pq.push({0, source});
+            sourceVertex = source;
+            distance[source] = 0ll;
+            priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
+            pq.push({0ll, source});
 
             while(!pq.empty()) {
-                int vertex = pq.top().second;
-                long long dist = pq.top().first;
+                auto [currDist, vertex] = pq.top();
                 pq.pop();
 
-                if(dist != distance[vertex]) continue;
+                if(currDist > distance[vertex]) continue;
 
                 for(auto edge: graph[vertex]) {
-                    int to = edge.first;
-                    int len = edge.second;
+                    ll to = edge.first;
+                    ll len = edge.second;
 
                     if(distance[vertex] + len < distance[to]) {
                         distance[to] = distance[vertex] + len;
@@ -55,11 +65,34 @@ class dijkstra{
             }
         }
 
-        vector<long long>& distances() {
+        bool hasPath(int v) {
+            return getDistanceToV(v) != INF;
+        }
+
+        vector<ll> distances() {
             return distance;
         }
 
-        vector<long long>& parents() {
+        vector<ll> parents() {
             return parent;
+        }
+
+        ll getDistanceToV(int x) {
+            return distance[x];
+        }
+
+        vector<long long> generatePathToV(int v) {
+            vector<long long> path;
+            if(getDistanceToV(v) == INF) {
+                return path;
+            }
+
+            while(v != sourceVertex) {
+                path.push_back(v);
+                v = parent[v];
+            }
+            path.push_back(sourceVertex);
+            reverse(all(path));
+            return path;
         }
 };
